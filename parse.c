d13671feb5c99c3d5c0ebf1521c4036dc7795c9a
massygh <massyghen1@gmail.com>
2023-12-19 06:03:06 -0800

    fix path

--- parse.c
+++ parse.c
@@ -1,48 +1,49 @@
 #include "shell.h"
 /**
-* parseLine - splits a string into words (tokens)
-* @line: the string to be splitted
-* Return: an array of strings (each word of the line string) or NULL
-*/
+ * parseLine - splits a string into words (tokens)
+ * @line: the string to be splitted
+ * Return: an array of strings (each word of the line string) or NULL
+ */
 char **parseLine(char *line)
 {
-	char *copy_line = NULL, *token = NULL;
-	char *delimiters = " \t\r\n\a";
-	char **tokens = NULL;
-	int count_token = 0, i = 0;
+	char *copy_line = NULL, *token = NULL; /* copy of the line -> input of users*/
+	char *delimiters = " \t\r\n\a"; /* delimiters of the line */
+	char **tokens = NULL; /* store the array of words */
+	int count_token = 0, i = 0; /* count the number of words and index */
 
 	copy_line = strdup(line);
-	if (!copy_line)
-		return (NULL);
+	if (!copy_line) /* if strdup fails */
+		return (NULL);  /* Count the number of words in the line using delimiters */
 	token = strtok(copy_line, delimiters);
 	while (token)
 	{
-		count_token++;
-		token = strtok(NULL, delimiters);
+		count_token++; /* increment the number of words */
+		token = strtok(NULL, delimiters); /* get the next word */
 	}
-	free(copy_line);
-	count_token++;
+	free(copy_line); /* free the copy of the line */
+	count_token++; /* increment the number of words for the NULL sign */
 	tokens = malloc(sizeof(char *) * count_token);
-	if (!tokens)
-		return (NULL);
+	if (!tokens) /* if malloc fails */
+		return (NULL); /* Duplicate the line again and split it into words */
 	copy_line = strdup(line);
-	if (!copy_line)
-	{	free(tokens);
+	if (!copy_line) /* if strdup fails */
+	{
+		free(tokens);
 		return (NULL);
-	}
+	} /* Split the line into words and store them in tokens */
 	token = strtok(copy_line, delimiters);
 	while (token)
-	{	tokens[i] = strdup(token);
-		if (!tokens[i])
+	{
+		tokens[i] = strdup(token);
+		if (!tokens[i]) /* if strdup fails */
 		{
 			free(tokens);
 			free(copy_line);
 			return (NULL);
-		}
-		i++;
+		} i++;
 		token = strtok(NULL, delimiters);
 	}
-	tokens[i] = NULL;
-	free(copy_line);
+	tokens[i] = NULL; /* Null-terminate the array of tokens */
+	free(copy_line); /* free the copy of the line */
 	return (tokens);
 }
--- path.c
+++ path.c
@@ -1,48 +1,51 @@
 #include "shell.h"
+
 /**
  * getPath - search for the file in the PATH
  * @file: the file to search for in the PATH
+ * Description: search for the and send the absolute path to the file if found
  * Return: the file path if found, or NULL
  */
 char *getPath(char *file)
 {
-	char *path = NULL;
-	char *file_path = NULL;
-	char *path_env = NULL;
+	char *path = NULL; /* to store the path */
+	char *file_path = NULL; /* to store the file path */
+	char *path_env = NULL; /* to store the env PATH */
+	/* struct stat to store the file info */
 	struct stat file_info;
-	int i;
-	char *token = strtok(path_env, ":");
+	int i; /* index for loop */
 
-	for (i = 0; file[i] != '\0'; i++)
+	for (i = 0; file[i] != '\0'; i++) /* loop to go through the file name */
 	{
-		if (file[i] == '/' || file[i] == '.')
+		if (file[i] == '/' || file[i] == '.') /* if contains '/' or '.' = PATH */
 		{
-			if (stat(file, &file_info) == 0)
-				return (strdup(file));
-			return (NULL);
+			if (stat(file, &file_info) == 0) /* check if file exists */
+				return (strdup(file)); /* return a copy of the file */
+			return (NULL); /* or return NULL if not found */
 		}
 	}
-	path = getEnv("PATH");
-	if (!path)
+	path = getEnv("PATH"); /* get the PATH environ variable */
+	if (!path) /* if getEnv fails or path is NULL */
 		return (NULL);
-	path_env = strdup(path);
-	if (path_env == NULL)
+	path_env = strdup(path); /* duplicate the PATH env variable and store it */
+	if (path_env == NULL) /* if strdup (duplicate) fails */
 		return (NULL);
-	while (token)
+	path = strtok(path_env, ":"); /* split the PATH env variable using ':' */
+	while (path) /* loop through the PATH as long as it's not NULL */
 	{
-		file_path = malloc(strlen(token) + strlen(file) + 2);
-		if (file_path == NULL)
-		{	free(path_env);
-			return (NULL);
+		/* allocate memory for the file path */
+		/* size of the path + size of the file + '/' + '\0' */
+		file_path = malloc(sizeof(char) * (strlen(path) + strlen(file) + 2));
+		/* concatenate the path with the file name using sprintf and / to separate */
+		sprintf(file_path, "%s/%s", path, file);
+		if (file_path != NULL && stat(file_path, &file_info) == 0)
+		{
+			free(path_env); /* if file exists or file_path is NULL free path_env */
+			return (file_path); /* return the file path */
 		}
-		sprintf(file_path, "%s/%s", token, file);
-		if (stat(file_path, &file_info) == 0)
-		{	free(path_env);
-			return (file_path);
-		}
-		free(file_path);
-		token = strtok(NULL, ":");
+		path = strtok(NULL, ":"); /* get the next path */
+		free(file_path); /* free the file path if file not found */
 	}
-	free(path_env);
-	return (NULL);
+	free(path_env); /* free the path_env */
+	return (NULL); /* return NULL if file not found */
 }
