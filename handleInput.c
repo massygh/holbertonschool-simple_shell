643b949893d8b3858872242f38c427cc12d77296
massygh <massyghen1@gmail.com>
2023-12-18 16:30:43 -0800

    fix betty

--- handleInput.c
+++ handleInput.c
@@ -7,24 +7,17 @@
  */
 int emptyLine(char *line)
 {
-	/* intialize only_spaces to true */
 	int only_spaces = true;
-	/* get the length of the line */
 	size_t len_line = strlen(line) - 1;
-	/* initialize i for loop */
 	size_t i;
 
-	/* loop through the line */
 	for (i = 0; i < len_line; i++)
 	{
-		/* if the line[i] is not a space or tab */
 		if (line[i] != TAB && line[i] != SPACE)
 		{
-			/* set only_spaces to false */
 			only_spaces = false;
-			break; /* break out of the loop */
+			break;
 		}
 	}
-	/* return only_spaces as 1 if true, 0 if false */
 	return (only_spaces);
 }
--- path.c
+++ path.c
@@ -3,17 +3,15 @@
 /**
  * getPath - search for the file in the PATH
  * @file: the file to search for in the PATH
- * Description: search for the and send the absolute path to the file if found
  * Return: the file path if found, or NULL
  */
 char *getPath(char *file)
 {
-	char *path = NULL; /* to store the path */
-	char *file_path = NULL; /* to store the file path */
-	char *path_env = NULL; /* to store the env PATH */
-	/* struct stat to store the file info */
+	char *path = NULL;
+	char *file_path = NULL;
+	char *path_env = NULL;
 	struct stat file_info;
-	int i; /* index for loop */
+	int i;
 
 	for (i = 0; file[i] != '\0'; i++) /* loop to go through the file name */
 	{
@@ -33,19 +31,16 @@ char *getPath(char *file)
 	path = strtok(path_env, ":"); /* split the PATH env variable using ':' */
 	while (path) /* loop through the PATH as long as it's not NULL */
 	{
-		/* allocate memory for the file path */
-		/* size of the path + size of the file + '/' + '\0' */
 		file_path = malloc(sizeof(char) * (strlen(path) + strlen(file) + 2));
-		/* concatenate the path with the file name using sprintf and / to separate */
 		sprintf(file_path, "%s/%s", path, file);
 		if (file_path != NULL && stat(file_path, &file_info) == 0)
 		{
 			free(path_env); /* if file exists or file_path is NULL free path_env */
 			return (file_path); /* return the file path */
 		}
-		path = strtok(NULL, ":"); /* get the next path */
-		free(file_path); /* free the file path if file not found */
+		path = strtok(NULL, ":");
+		free(file_path); /* free the file */
 	}
-	free(path_env); /* free the path_env */
-	return (NULL); /* return NULL if file not found */
+	free(path_env);
+	return (NULL);
 }
