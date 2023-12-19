c05319c2c918ea0cc0a50a735f243b0c5887402c
massygh <massyghen1@gmail.com>
2023-12-19 06:23:02 -0800

    fix path

--- path.c
+++ path.c
@@ -8,29 +8,18 @@
  */
 char *getPath(char *file)
 {
-	char *path = NULL; /* to store the path */
-	char *file_path = NULL; /* to store the file path */
-	char *path_env = NULL; /* to store the env PATH */
-	/* struct stat to store the file info */
-	struct stat file_info;
-	int i; /* index for loop */
+	char *path = NULL;          /* to store the path */
+	char *file_path = NULL;     /* to store the file path */
+	char *path_env = getEnv("PATH"); /* get the PATH environ variable */
 
-	for (i = 0; file[i] != '\0'; i++) /* loop to go through the file name */
-	{
-		if (file[i] == '/' || file[i] == '.') /* if contains '/' or '.' = PATH */
-		{
-			if (stat(file, &file_info) == 0) /* check if file exists */
-				return (strdup(file)); /* return a copy of the file */
-			return (NULL); /* or return NULL if not found */
-		}
-	}
-	path = getEnv("PATH"); /* get the PATH environ variable */
-	if (!path) /* if getEnv fails or path is NULL */
+	if (!path_env) /* if getEnv fails or path is NULL */
 		return (NULL);
-	path_env = strdup(path); /* duplicate the PATH env variable and store it */
-	if (path_env == NULL) /* if strdup (duplicate) fails */
+
+	path = strdup(path_env); /* duplicate the PATH env variable and store it */
+	if (!path) /* if strdup (duplicate) fails */
 		return (NULL);
-	path = strtok(path_env, ":"); /* split the PATH env variable using ':' */
+
+	path = strtok(path, ":"); /* split the PATH env variable using ':' */
 	while (path) /* loop through the PATH as long as it's not NULL */
 	{
 		/* allocate memory for the file path */
@@ -38,14 +27,21 @@ char *getPath(char *file)
 		file_path = malloc(sizeof(char) * (strlen(path) + strlen(file) + 2));
 		/* concatenate the path with the file name using sprintf and / to separate */
 		sprintf(file_path, "%s/%s", path, file);
-		if (file_path != NULL && stat(file_path, &file_info) == 0)
+
+		/* check if the file exists and is executable */
+		struct stat file_info;
+
+		if (file_path != NULL && stat(file_path, &file_info) == 0
+				&& S_ISREG(file_info.st_mode) && (file_info.st_mode & S_IXUSR))
 		{
-			free(path_env); /* if file exists or file_path is NULL free path_env */
+			free(path);       /* free the path */
 			return (file_path); /* return the file path */
 		}
+
+		free(file_path); /* free the file path if file not found or not executable */
 		path = strtok(NULL, ":"); /* get the next path */
-		free(file_path); /* free the file path if file not found */
 	}
-	free(path_env); /* free the path_env */
-	return (NULL); /* return NULL if file not found */
+
+	free(path); /* free the path */
+	return (NULL); /* return NULL if file not found or not executable */
 }
